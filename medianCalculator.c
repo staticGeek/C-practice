c4eddc9d475dd6dbe8dfbc4629ab9245d572f340
goutammukherjee214@gmail.com <goutammukherjee214@gmail.com>
2021-01-26 21:32:20 +0530

    second commit

---
+++ medianCalculator.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+// swap function
+void swap(float *first, float *second)
+{
+    float temp;
+    temp = *first, *first = *second, *second = temp;
+}
+
+// sorting given array
+void sortArr(int count, float *arr)
+{
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 0; j < count - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+}
+
+// median calculator function
+float medianCalculator(int count, float *sorted_arr)
+{
+    if (count % 2 == 0)
+        return (sorted_arr[(count - 1) / 2] + sorted_arr[count / 2]) / 2.0;
+    return sorted_arr[count / 2];
+}
+
+int main()
+{
+    int count;
+    printf("How many numbers do you wanna input? : ");
+    scanf("%d", &count);
+    float n[count - 1];
+
+    // taking inputs
+    for (int i = 0; i < count; i++)
+    {
+        printf("Enter value: ");
+        if (scanf("%f", &n[i]) == 0)
+        {
+            printf("Wrong input. program terminated");
+            return -1;
+        }
+    }
+
+    // sorting the array
+    sortArr(count, n);
+
+    // calculating median
+    float median;
+    median = medianCalculator(count, n);
+
+    // printing the median
+    printf("The median of the given data is: %f", median);
+
+    return 0;
+}
